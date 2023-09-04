b9eddf0e70d33753eb32b1a3376e836612424fad
Gabriel Romano <64925550+gabrielromanosec@users.noreply.github.com>
2023-09-04 13:27:48 -0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,15 +2,15 @@ const { Worker } = require('bullmq');
 
 
 const worker = new Worker('filaConsulta', async job => {
-  const { placa } = job.data;
+  const { cpf } = job.data;
 
   if(job.name == "consulta"){
     //Aqui pode se fazer o processo de validação ou abrir mais jobs
   }
   
-  console.log(`${job.name}Consultando cpf: ${placa}`);
+  console.log(`${job.name}Consultando cpf: ${cpf}`);
   await new Promise(resolve => setTimeout(resolve, 1000)); 
-  console.log(`Consulta concluída para cpf: ${placa}`);
+  console.log(`Consulta concluída para cpf: ${cpf}`);
   
-  return { resultado: 'Informações da cpf', placa };
+  return { resultado: 'Informações da cpf', cpf };
 });
